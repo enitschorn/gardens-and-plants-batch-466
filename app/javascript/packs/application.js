0b8d62656145eace3f63f01e619143cb8e20e908
Christine Schorn <cschorn91@gmail.com>
2021-11-20 17:25:12 +1100

    Rails frontend module livecode

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,21 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import 'bootstrap';
+import 'select2/dist/css/select2.css';
+
+import { initSelect2 } from '../components/init_select2';
+
+document.addEventListener('turbolinks:load', () => {
+  initSelect2();
+})
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :gardens do
+    resources :plants, only: [:new, :create]
+  end
+
+  resources :plants, only: :destroy do
+    resources :plant_tags, only: [ :new, :create ]
+  end
+end
